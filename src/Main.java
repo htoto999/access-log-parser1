38cf8c0418c7ad677a3a73d552802d9dd62a2c5e
Htoto999 <htoto999@gmail.com>
2023-10-08 13:28:29 +0300

    task_2_numbers_operations

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in) .nextLine();
-        System.out.println("Длинна текста: "+ text.length());
+        System.out.println("Введите первое число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+
+        System.out.println("Введите второе число:");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        int resultSum = firstNumber + secondNumber;
+        System.out.println("Сумма первого и второго чисел: "+resultSum);
+
+        int resultSubstract = firstNumber - secondNumber;
+        System.out.println("Результат вычитания второго числа из первого: "+resultSubstract);
+
+        int resultMultiply = firstNumber * secondNumber;
+        System.out.println("Произведение первого и второго чиселы: "+resultMultiply);
+
+        double resultQuotient = (double) firstNumber / secondNumber;
+        System.out.println("Результат деления первого числа на второе: "+resultQuotient);
+
     }
 }
